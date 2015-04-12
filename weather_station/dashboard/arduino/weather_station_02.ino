daa65e8eaddd8094eaacc3e31181f24c64158bec
Darren Douglas Lee aka Dee <darrendouglas.lee@gmail.com>
2015-04-11 22:55:48 -0400

    Dew Point

--- weather_station/dashboard/arduino/weather_station_02.ino
+++ weather_station/dashboard/arduino/weather_station_02.ino
@@ -38,12 +38,19 @@ void loop() {
   // Read temperature as Celsius
   float c = dht.readTemperature();
 
+  // Read temperature as Celsius
+  float k = c + 273.15;
+
   // Read temperature as Fahrenheit
   float f = dht.readTemperature(true);
 
   // Compute heat index
   // Must send in temp in Fahrenheit!
   float hi = dht.computeHeatIndex(f, h);
+
+  // How cold? (temperature and humidity with Arduino)
+  // http://wodieskodie.com/how-cold-temperature-and-humidity-with-arduino/
+  float dp = dewPoint(c, h);
 
   if (client) {
     // read the command
@@ -59,6 +66,11 @@ void loop() {
       client.print(c);
       //client.print(" degrees C");
      }
+     if (request == "tempk") {
+      //client.print("<br>Current Temperature: ");
+      client.print(k);
+      //client.print(" degrees K");
+     }
      if (request == "hum") {
       //client.print("<br>Current Humidity: ");
       client.print(h);
@@ -66,6 +78,10 @@ void loop() {
      if (request == "hi") {
       //client.print("<br>Heat Index: ");
       client.print(hi);
+     }
+     if (request == "dp") {
+      //client.print("<br>Dew Point: ");
+      client.print(dp);
      }
      if (request == "light") {
       //client.print("<br>Light: ");
@@ -85,4 +101,24 @@ void loop() {
     client.stop();
   }
   delay(50);
+}
+
+// ****************
+// Declare User-written Functions
+// ****************
+
+// dewPoint function NOAA
+// reference: http://wahiduddin.net/calc/density_algorithms.htm
+double dewPoint(double celsius, double humidity) {
+
+  double A0= 373.15/(273.15 + celsius);
+  double SUM = -7.90298 * (A0-1);
+  SUM += 5.02808 * log10(A0);
+  SUM += -1.3816e-7 * (pow(10, (11.344*(1-1/A0)))-1) ;
+  SUM += 8.1328e-3 * (pow(10,(-3.49149*(A0-1)))-1) ;
+  SUM += log10(1013.246);
+  double VP = pow(10, SUM-3) * humidity;
+  double T = log(VP/0.61078); // temp var
+
+  return (241.88 * T) / (17.558-T);
 }
